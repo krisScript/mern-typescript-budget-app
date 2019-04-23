f2ee4ed30664eb5a9b0f02229b2ab08ad7d784ef
Kristiyan Todorov <kristiyantkirchev@gmail.com>
2019-04-23 13:30:31 +0300

    added SignUpForm

--- client/src/components/SignUpForm/SignUpForm.tsx
+++ client/src/components/SignUpForm/SignUpForm.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent, useState, SyntheticEvent } from 'react';
 import { NavLink } from 'react-router-dom';
 import Input from '../Input/Input';
 const SignUpForm: FunctionComponent = (): JSX.Element => {
@@ -7,8 +7,11 @@ const SignUpForm: FunctionComponent = (): JSX.Element => {
   const [password, setPassword] = useState<string>('');
   const [matchPassword, setMatchPassword] = useState<string>('');
   const validationErrorParams: string[] = [];
+  const submitHandler = (e: SyntheticEvent) => {
+    e.preventDefault();
+  };
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <Input
         name="username"
         value={username}
@@ -17,6 +20,38 @@ const SignUpForm: FunctionComponent = (): JSX.Element => {
         placeholder="Enter your username"
         validationErrorParams={validationErrorParams}
       />
+      <Input
+        name="email"
+        value={username}
+        setValueHook={setUsername}
+        type="email"
+        placeholder="Enter your email"
+        validationErrorParams={validationErrorParams}
+      />
+      <Input
+        name="password"
+        value={username}
+        setValueHook={setUsername}
+        type="password"
+        placeholder="Enter your password"
+        validationErrorParams={validationErrorParams}
+      />
+      <Input
+        name="matchPassword"
+        value={username}
+        setValueHook={setUsername}
+        type="password"
+        placeholder="Repeat your username"
+        validationErrorParams={validationErrorParams}
+      />
+      <div className="field is-grouped">
+        <div className="control">
+          <button className="button is-link">Submit</button>
+        </div>
+        <div className="control">
+          <button className="button is-text">Cancel</button>
+        </div>
+      </div>
     </form>
   );
 };
--- client/src/util/getInputError.ts
+++
@@ -1,5 +0,0 @@
-const getInputError = (validationErrorParams: string[], name: string) => {
-  return validationErrorParams.includes(name);
-};
-
-export default getInputError;
