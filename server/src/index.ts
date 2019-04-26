eace47be24d8d65ce8a03a6e87055cccb475253e
Kristiyan Todorov <kristiyantkirchev@gmail.com>
2019-04-26 09:26:54 +0300

    added helmet

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,12 +3,14 @@ import { Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 import mongoose, { Error } from 'mongoose';
 import authRoutes from './routes/auth';
+import helmet from 'helmet';
 const app = express();
 
 const mongoURI: string = `mongodb+srv://${process.env.MONGO_USER}:${
   process.env.MONGO_PASSWORD
 }@cluster0-zmcyw.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true`;
 
+app.use(helmet);
 app.use(bodyParser.json()),
   app.use(
     (req: Request, res: Response, next: NextFunction): void => {
